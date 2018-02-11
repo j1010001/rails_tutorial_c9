6a0106d5068c14d56c99491b0ba27ee66e24108c
Jan Bernatik <jan.bernatik@gmail.com>
2018-02-11 22:08:33 +0000

    changing help_path to helf_path using alias

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   get 'home', to: 'static_pages#home'
 
-  get  '/help', to: 'static_pages#help'
+  get  '/help', to: 'static_pages#help', as: 'helf'
 
   get 'about', to: 'static_pages#about'
   
