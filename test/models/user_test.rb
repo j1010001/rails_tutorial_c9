d6fcdfcbd651ddcdbb1b6da69fd204781177e8be
Jan Bernatik <jan.bernatik@gmail.com>
2018-02-12 04:33:04 +0000

    added validations for user name and email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  # special setup method automatically gets run before each test.
+  #   Because @user is an instance variable, it’s automatically available 
+  #   in all the tests, and we can test its validity using the valid? method
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+  
 end
